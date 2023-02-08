9ba2a035bbe9ece25c84e7a22d5a1f9df15daee3
ToddGallegos <toroga@hotmail.com>
2023-02-07 18:14:36 -0800

    updated routes and links

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react'
 import Home from './Home';
 import Nav from './Nav';
 import Pokemon from './Pokemon';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        <Home />
-        <Pokemon />
-      </div>
+      <BrowserRouter>
+
+        <div>
+          <Nav />
+          <Routes>
+
+            <Route path='/' element={<Home />}/>
+            <Route path='/pokemon' element={<Pokemon />}/>
+            
+          </Routes>
+
+        </div>
+
+      </BrowserRouter>
     )
   }
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom';
 
 export default class Nav extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="o">Todd's React</a>
+                    <Link className="navbar-brand" to="/">Todd's React</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="o">Home</a>
-                            <a className="nav-link" href="o">Pokemon</a>
-                            <a className="nav-link" href="o">RandomLink</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link" to="/pokemon">Pokemon</Link>
+                            <a className="nav-link" href="https://www.google.com" target="_blank" rel="noopener noreferrer">Google</a>
                         </div>
                     </div>
                 </div>
